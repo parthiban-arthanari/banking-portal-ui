a00473db14a9fa9fa3ca20457a39a158b8fb9c6a
parthiban-arthanari <parthi.viral@gmail.com>
2025-02-09 23:24:56 +0530

    url updated

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://banking-portal-api-parthi-viral-dev.apps.rm1.0a51.p1.openshiftapps.com/api',
+  apiUrl: 'ttps://banking-portal-api-0-naveennavy219-dev.apps.rm2.thpm.p1.openshiftapps.com/api',
   tokenName :  "authToken",
   origin:'http://localhost:4200' 
 };
